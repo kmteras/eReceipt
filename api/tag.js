5e80d088d3060da6b018ef8c60cfa42803d2d0c9
teras <karlnitram@gmail.com>
2018-02-06 21:16:07 +0200

    Tag validation

--- api/tag.js
+++ api/tag.js
@@ -1,6 +1,20 @@
 'use strict';
 
 module.exports = class Tag {
+    static get validator(){
+        return {
+            type: 'object',
+            properties: {
+                name: {
+                    type: 'string'
+                }
+            },
+            required: [
+                'name'
+            ]
+       }
+    };
+
     constructor(db) {
         this.db = db;
         const that = this;
@@ -10,6 +24,15 @@ module.exports = class Tag {
             }
             else {
                 that.database = database;
+                that.setup(database);
+            }
+        });
+    }
+
+    setup(db) {
+        db.createCollection("tags", {validator: Tag.validator}, function(err, result) {
+            if(err) {
+                throw err;
             }
         });
     }
@@ -23,22 +46,20 @@ module.exports = class Tag {
     post(req, res) {
         if(req.body['name'] === undefined) {
             res.status(500);
-            res.json({'error': 'Tag name is not defined'});
+            res.json({error: 'Tag name is not defined'});
             return;
         }
 
         let tag = {
-            'name': req.body['name']
+            name2: req.body['name']
         };
-
-        
 
         this.database.collection('tags').insertOne(tag, function(err, result) {
             if(err === null) {
-                res.json({'error': null});
+                res.json({error: null});
             }
             else {
-                res.json({});
+                res.json({error: err});
             }
         });
     }
