f75c578e1a5650d3d11cbdae5a42518775c7fe17
teras <karlnitram@gmail.com>
2018-02-06 14:40:52 +0200

    Connect to mongodb

--- application.js
+++ application.js
@@ -1,4 +1,16 @@
-var http = require('http');
+const http = require('http');
+const mongoClient = require('mongodb').MongoClient;
+
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'ereceipt';
+
+mongoClient.connect(dbUrl, function(err, client) {
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    client.close();
+});
 
 var app = http.createServer(function(req, res) {
     if(req.url.startsWith("/api")) {
