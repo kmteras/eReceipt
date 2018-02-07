faaf4424bece1cfe5582ace51774294b80a5798b
teras <karlnitram@gmail.com>
2018-02-07 16:22:14 +0200

    Can add tags to receipts

--- api/receipt_tag.js
+++ api/receipt_tag.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = class ReceiptTag {
     constructor(db) {
         this.db = db;
@@ -22,14 +24,34 @@ module.exports = class ReceiptTag {
         const that = this;
 
         this.database.collection('tags').insertOne({ name: req.body.tag_name }, function(err, result) {
-            if(err) {
-                throw err;
-            }
-            console.log(req.body.tag_name);
-            that.database.collection('receipts').updateOne({ _id: req.body.receipt_id },
-                { $set: { tags: [req.body.tag_name] }}, function(err, result) {
-                res.json({error: err});
-            });
+                //Error 11000 - duplicate tag, we don't care about that
+                if(err && err.code !== 11000) {
+                    res.json(err);
+                }
+                else {
+
+
+                    console.log(req.body.receipt_id);
+
+                    that.database.collection('receipts').find().toArray(function(err, docs) {
+                        console.log(docs);
+                    });
+
+                    that.database.collection('receipts').updateOne({ _id: new ObjectID(req.body.receipt_id) },
+                        {
+                            $set: {
+                                tags:
+                                    [
+                                        {
+                                            name: req.body.tag_name
+                                        }
+                                    ]
+                            }
+                        },
+                        { upsert: true }, function (err, result) {
+                            res.json({error: err, result: result});
+                        });
+                }
         });
     }
 };
