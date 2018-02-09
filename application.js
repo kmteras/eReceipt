1e8c4315c3eca1c8362de96d42ab65df58f4fc03
Rao Zvorovski <rao.zvorovski@outlook.com>
2018-02-09 08:44:48 +0200

    Somewhat improve performance

--- application.js
+++ application.js
@@ -5,9 +5,12 @@ const db = require('./database.js');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
+const compression = require('compression');
 
 const helmet = require('helmet');
 const cors = require('cors');
+
+const spdy = require('spdy');
 
 const sni = require('sni-reader');
 const net = require('net');
@@ -16,6 +19,7 @@ const tag = new (require('./api/tag.js'))(db);
 const search = new (require('./api/search.js'))(db);
 const receipt = new (require('./api/receipt.js'))(db);
 const receipt_tag = new (require('./api/receipt_tag.js'))(db);
+
 
 const privateKey = fs.readFileSync('ssl/server.key', 'utf8');
 const publicKey = fs.readFileSync('ssl/server.crt', 'utf8');
@@ -36,11 +40,18 @@ const app2 = express();
 
 
 app.use(function(req, res, next) {
-    if(req.url == "/receipt"){
+    console.log(req.url);
+    if(req.url.startsWith("/receipts")){
+        req.url = '/';
+    }
+    if(req.url.startsWith("/statistics")){
         req.url = '/';
     }
     next();
 });
+
+app.use(compression());
+app2.use(compression());
 
 app.use(helmet());
 app.use(cors());
@@ -50,6 +61,8 @@ app.use(express.static('eReceipt-front/dist/'));
 app2.use(express.static('eReceipt-front/simple_html/landing_page'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
+const longpoll =  require("express-longpoll")(app);
 
 app.get('/api/receipt/', (req, res) => receipt.get(req, res));
 app.post('/api/receipt/', (req, res) => receipt.post(req, res));
@@ -72,8 +85,8 @@ app.get('/api/whoami', (req, res) => {
 
 app.listen(3004, () => console.log("Http testing server running on port 3004"));
 
-const httpsServer = https.createServer(httpsNoAuth, app2);
-const httpsAuthServer = https.createServer(httpsAuth, app);
+const httpsServer = spdy.createServer(httpsNoAuth, app2);
+const httpsAuthServer = spdy.createServer(httpsAuth, app);
 
 httpsServer.listen(3001, () => console.log('Noauth Server running on port 3001'));
 httpsAuthServer.listen(3002, () => console.log('Auth server running on port 3002'));
